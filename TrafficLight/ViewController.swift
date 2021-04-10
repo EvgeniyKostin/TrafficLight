ddd5d55f8d3bab1929060aaf1e901c1537a6b4e9
Evgeniy Kostin <kostinen@icloud.com>
2021-04-11 00:19:30 +0300

    completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,53 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var trafficLightRedView: UIView!
-    @IBOutlet weak var trafficLightYellowView: UIView!
-    @IBOutlet weak var trafficLightGreenView: UIView!
-    
-    @IBOutlet weak var startButton: UIButton!
-    
-    let dimLight: CGFloat = 0.2
-    let fullBrightness: CGFloat = 1.0
-    var modes = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        startButton.layer.cornerRadius = 10
-//        trafficLightRedView.layer.cornerRadius = 120
-//        trafficLightYellowView.layer.cornerRadius = 120
-//        trafficLightGreenView.layer.cornerRadius = 120
-        
-        startButton.setTitle("START", for: .normal)
-    }
-
-    @IBAction func startButtonPressed() {
-        switch modes {
-        case 0:
-            trafficLightRedView.alpha = fullBrightness
-            trafficLightYellowView.alpha = dimLight
-            trafficLightGreenView.alpha = dimLight
-            startButton.setTitle("NEXT", for: .normal)
-            modes = 1
-        case 1:
-            trafficLightYellowView.alpha = fullBrightness
-            trafficLightRedView.alpha = dimLight
-            modes = 2
-        case 2:
-            trafficLightYellowView.alpha = dimLight
-            trafficLightGreenView.alpha = fullBrightness
-            modes = 3
-        case 3:
-            trafficLightYellowView.alpha = dimLight
-            trafficLightGreenView.alpha = dimLight
-            startButton.setTitle("START", for: .normal)
-            modes = 0
-        default:
-            break
-        }
-    }
-    
+enum CurrentLight {
+    case red, yellow, green
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var startButton: UIButton!
+
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startButton.layer.cornerRadius = 10
+
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
+
+        startButton.layer.shadowColor = UIColor.blue.cgColor
+        startButton.layer.shadowOpacity = 1
+        startButton.layer.shadowOffset = .zero
+        startButton.layer.shadowRadius = 5
+        
+        redLightView.layer.shadowColor = UIColor.red.cgColor
+        redLightView.layer.shadowOpacity = 2
+        redLightView.layer.shadowOffset = .zero
+        redLightView.layer.shadowRadius = 10
+        
+        yellowLightView.layer.shadowColor = UIColor.yellow.cgColor
+        yellowLightView.layer.shadowOpacity = 2
+        yellowLightView.layer.shadowOffset = .zero
+        yellowLightView.layer.shadowRadius = 10
+        
+        greenLightView.layer.shadowColor = UIColor.green.cgColor
+        greenLightView.layer.shadowOpacity = 2
+        greenLightView.layer.shadowOffset = .zero
+        greenLightView.layer.shadowRadius = 10
+    }
+
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = redLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = redLightView.frame.width / 2
+    }
+
+    private func colorSettings() {
+        
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLightView.alpha = lightIsOn
+            yellowLightView.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+}
